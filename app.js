51b84e45d235cf3b9d6867d0bf50a0f4f4658496
Serdar Halac <serdarhalax@gmail.com>
2018-04-02 23:20:14 -0700

    Added npm script

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 // bring in routers
-const all_routes = require('./routes/all');
-const user_routes = require('./routes/user');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
