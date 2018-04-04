3730daf224ccf29024fb19a287140bce66ba3a23
Serdar Halac <serdarhalax@gmail.com>
2018-04-04 01:01:19 -0700

    Added CSS, images and banner

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
